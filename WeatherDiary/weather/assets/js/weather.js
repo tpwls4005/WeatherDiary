dd047222c1d74e49c0b124885317ea997abd1e7c
김효은 <hyoeun@gmail.com>
2024-02-28 20:49:05 +0900

    오늘의 날씨 api 적용

--- WeatherDiary/weather/assets/js/weather.js
+++ WeatherDiary/weather/assets/js/weather.js
@@ -7,29 +7,37 @@ const getWeather = async () => {
         const weatherData = await fetch(url);
         const data = await weatherData.json();
 
-        const temperature = data.DailyForecasts[0];
-        console.log(temperature);
-        const weather = data;
         weatherList = data.DailyForecasts
-        console.log("weather 테스트", weather);
-        todayRender(weather);
+        console.log(weatherList);
+        todayRender();
     } catch (error) {
         console.error('날씨 데이터를 받아오는데 실패했습니다.', error);
     }
 };
 
-const todayRender = (weather) => {
-    const todayWeatherHTML = `
-        <div class="today_address">서울특별시</div>
-        <div class="today_cont"></div>
-        <div class="today_temperature">${weather.temperature}</div>
-        <ul class="today_chart_list">
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <div class="today_style"></div>`;
-    document.querySelector('.today_weather').innerHTML = todayWeatherHTML;
+const todayRender = () => {
+    const weatherDiv = document.querySelector('.today_weather'); // HTML에서 날씨 정보를 보여줄 요소를 선택
+    weatherDiv.innerHTML = ''; // 이전의 날씨 정보를 초기화
+
+    const forecast = weatherList[0]; // 첫 번째 날씨 예보 정보 가져오기
+    const minTemperature = Math.floor(forecast.Temperature.Minimum.Value / 10); // 최저 온도에 Math.floor() 함수 적용
+    const maxTemperature = Math.floor(forecast.Temperature.Maximum.Value / 10); // 최고 온도에 Math.floor() 함수 적용
+
+    const forecastDetails = `<div class="today_address">서울특별시</div> 
+            <div class="today_cont">${forecast.Day.IconPhrase}</div>       
+            <div class="today_temperature">${forecast.Day.Icon}</div>
+            <div class="today_addTemperature"><span class="min">${minTemperature}°</span> / <span class="max">${maxTemperature}°</span></div>
+                <ul>
+                    <li>습도 : ${forecast.Day.WetBulbTemperature.Average.Value}</li>
+                    <li>바람 : ${forecast.Day.Wind.Speed.Value}  ${forecast.Day.Wind.Direction.Localized}</li>
+                    <li>자외선 : ${forecast.AirAndPollen[5].Value} </li>
+                </ul>
+            <div class="today_style"></div>
+
+    `;
+    weatherDiv.innerHTML = forecastDetails; // 가져온 날씨 정보를 HTML 요소에 넣기
 };
+
+
 
 getWeather();

206a90109750ad3da6fb9ee5b25a8456f260c644
김효은 <hyoeun@gmail.com>
2024-02-29 21:07:48 +0900

    온도 전체 수정 작업

--- WeatherDiary/weather/assets/js/weather.js
+++ WeatherDiary/weather/assets/js/weather.js
@@ -1,47 +1,60 @@
 const API_KEY = `tdA81qDc4LAVznQDrQKG9zv0kmexr8kL`;
 let weatherList = [];
 
-const getWeather = async () => {
+const fetchWeatherData = async (url, dataHandler, renderCallback) => {
     try {
-        const url = new URL(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/226081?apikey=${API_KEY}&language=ko-kr&details=true`);
-        const weatherData = await fetch(url);
-        const data = await weatherData.json();
+        const response = await fetch(url);
+        const data = await response.json();
 
-        weatherList = data.DailyForecasts
+        // 데이터 처리 콜백을 통해 데이터 가공
+        weatherList = dataHandler(data);
+
         console.log(weatherList);
-        todayRender();
+        renderCallback();
     } catch (error) {
         console.error('날씨 데이터를 받아오는데 실패했습니다.', error);
     }
 };
 
-const getWeekWeather = async () => {
-    try {
-        const url = new URL(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/226081?apikey=${API_KEY}&language=ko-kr&details=true`);
-        const weekWeatherData = await fetch(url);
-        const data = await weekWeatherData.json();
-        weatherList = data.DailyForecasts
-
-        console.log(weatherList);
-        weekendRender();
-    } catch (error) {
-        console.error('주간 날씨 데이터를 받아오는데 실패했습니다.', error);
-    }
+const getCurrentWeather = () => {
+    const url = `http://dataservice.accuweather.com/currentconditions/v1/226081?apikey=${API_KEY}&language=ko-kr&details=true&metric=true`;
+    fetchWeatherData(url, data => data, todayRender);
 };
 
+const getWeather = () => {
+    const url = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/226081?apikey=${API_KEY}&language=ko-kr&details=true&metric=true`;
+    fetchWeatherData(url, data => data.DailyForecasts, todayAddRender);
+};
 
+const getWeekWeather = () => {
+    const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/226081?apikey=${API_KEY}&language=ko-kr&details=true&metric=true`;
+    fetchWeatherData(url, data => data.DailyForecasts, weekendRender);
+};
 
 const todayRender = () => {
-    const todayWeather = document.querySelector('.today_weather'); // HTML에서 날씨 정보를 보여줄 요소를 선택
+    const todayWeather = document.querySelector('.today'); // HTML에서 날씨 정보를 보여줄 요소를 선택
     todayWeather.innerHTML = ''; // 이전의 날씨 정보를 초기화
 
     const forecast = weatherList[0]; // 첫 번째 날씨 예보 정보 가져오기
-    const minTemperature = Math.floor(forecast.Temperature.Minimum.Value / 10); // 최저 온도에 Math.floor() 함수 적용
-    const maxTemperature = Math.floor(forecast.Temperature.Maximum.Value / 10); // 최고 온도에 Math.floor() 함수 적용
-
+    const Temperature = Math.round((forecast.ApparentTemperature.Metric.Value));
+    
     const forecastDetails = `<div class="today_address">서울특별시</div> 
-            <div class="today_cont">${forecast.Day.IconPhrase}</div>       
-            <div class="today_temperature">${forecast.Day.Icon}</div>
+    <div class="today_cont">${forecast.WeatherText}</div>       
+    <div class="today_temperature">${Temperature}</div>
+    `;
+    todayWeather.innerHTML = forecastDetails; // 가져온 날씨 정보를 HTML 요소에 넣기
+}
+
+const todayAddRender = () => {
+    const todayAddWeather = document.querySelector('.today_add'); // HTML에서 날씨 정보를 보여줄 요소를 선택
+    todayAddWeather.innerHTML = ''; // 이전의 날씨 정보를 초기화
+
+    const forecast = weatherList[0]; // 첫 번째 날씨 예보 정보 가져오기
+
+    const minTemperature =  Math.round((forecast.Temperature.Minimum.Value));
+    const maxTemperature =  Math.round((forecast.Temperature.Maximum.Value));
+
+    const forecastAddDetails = `
             <div class="today_addTemperature"><span class="min">${minTemperature}°</span> / <span class="max">${maxTemperature}°</span></div>
                 <ul class="d-flex justify-content-center align-items-center">
                     <li>습도 : ${forecast.Day.WetBulbTemperature.Average.Value}</li>
@@ -49,9 +62,30 @@ const todayRender = () => {
                     <li>자외선 : ${forecast.AirAndPollen[5].Value} </li>
                 </ul>
             <div class="today_style"></div>
-
     `;
-    todayWeather.innerHTML = forecastDetails; // 가져온 날씨 정보를 HTML 요소에 넣기
+    todayAddWeather.innerHTML = forecastAddDetails; // 가져온 날씨 정보를 HTML 요소에 넣기
+};
+
+const getImageSrc = (IconPhrase) => {
+    if (IconPhrase.includes('맑음')) {
+        return '../weather/assets/image/icon_sun.svg';
+    } else if (IconPhrase.includes('흐림')) {
+        return '../weather/assets/image/icon_cloud.svg';
+    } else if (IconPhrase.includes('안개')) {
+        return '../weather/assets/image/icon_mist.svg';
+    } else if (IconPhrase.includes('비')) {
+        return '../weather/assets/image/icon_rain.svg';
+    } else if (IconPhrase.includes('눈')) {
+        return '../weather/assets/image/icon_snow.svg';
+    } else if (IconPhrase.includes('소나기')) {
+        return '../weather/assets/image/icon_shower.svg';
+    } else if (IconPhrase.includes('번개')) {
+        return '../weather/assets/image/icon_lightning.svg';
+    } else if(IconPhrase.includes('짙은구름')) {
+        return '../weather/assets/image/icon_cloud2.svg';
+    } else if(IconPhrase.includes('추움') || IconPhrase.includes('바람')) {
+        return '../weather/assets/image/icon_wind.svg';
+    }
 };
 
 const weekendRender = () => {
@@ -72,16 +106,19 @@ const weekendRender = () => {
         const day = days[date.getDay()];  // getDay()는 0(일요일) ~ 6(토요일) 값을 반환
         const dateString = date.getTime() === today.getTime() ? '오늘' : (date.getMonth()+1) + '월 ' + date.getDate() + '일';  // 월과 일을 문자열로 만듦
 
-        const minTemperature = Math.floor(forecast.Temperature.Minimum.Value / 10); // 최저 온도에 Math.floor() 함수 적용
-        const maxTemperature = Math.floor(forecast.Temperature.Maximum.Value / 10); // 최고 온도에 Math.floor() 함수 적용
+        const minTemperature =  Math.round((forecast.Temperature.Minimum.Value));
+        const maxTemperature =  Math.round((forecast.Temperature.Maximum.Value));
+
+        const imageSrc = getImageSrc(forecast.Day.IconPhrase);
+        const nightImageSrc = getImageSrc(forecast.Night.IconPhrase);
 
         return `
             <li class="week_item col-lg-6 d-flex justify-content-between align-items-center">
                 <div class="cell_date d-flex flex-column align-items-center">
                      ${day}<span>${dateString}</span>
                 </div>
-                <div class="cell_icon"></div>
-                <div class="cell_icon"></div>
+                <div class="cell_icon"><img src="${imageSrc}">${forecast.Day.IconPhrase}</div>
+                <div class="cell_icon"><img src="${nightImageSrc}">${forecast.Night.IconPhrase}</div>
                 <div class="cell_temperature_lowest min">${minTemperature}°</div>
                 <div class="cell_temperature_highest max">${maxTemperature}°</div>
             </li>
@@ -91,5 +128,6 @@ const weekendRender = () => {
     weekWeather.innerHTML = weekForecastDetail; // 가져온 날씨 정보를 HTML 요소에 넣기
 }
 
+getCurrentWeather();
 getWeather();
 getWeekWeather();

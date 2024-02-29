0a3b9406d89c19699300eae5fce8d525fb318a57
김주영 <marine@jy.local>
2024-02-29 10:41:07 +0900

    일기

--- weather/assets/js/diary.js
+++ weather/assets/js/diary.js
@@ -1,4 +1,4 @@
-// 유저가 캘린더에서 날짜를 선택하면 해당하는 날짜에 날씨가 반영되고, 일기를 작성할 수 있다.
+// 유저가 캘린더에서 날짜를 선택하면 해당하는 날짜에 등록된 일기를 볼 수 있다.
 // 일기를 작성하고, 감정 이모티콘을 선택하여 그 날의 기분을 선택할 수 있다.
 // 등록하기 버튼을 눌러 해당 일자에 일기를 등록할 수 있다.
 // 등록이 완료된 일기는 삭제하기 버튼을 눌러 삭제할 수 있다.
@@ -8,22 +8,31 @@
 let diaryText = document.getElementById("exampleFormControlTextarea1")
 let addButton = document.getElementById("submit_btn")
 let deleteButton = document.getElementById("delete_btn")
-// addButton.disabled = true
+let emotionContent = document.querySelector(".emotion_content")
+
 let diaryList = []
-
-
-// diaryText.addEventListener("input", disabledAddButton)
+let myEmotion = ""
 
 const addDiary = () => {
     let diaryObject = diaryText.value
-    diaryList.push(diaryObject)
-    // addButton.disabled = true
-    console.log("찍혀요?!",diaryObject)
+    diaryList.push({ content: diaryObject, emotion: myEmotion })
+    
+    console.log(`일기 내용 : ${diaryObject}`)
+    console.log(`감정 : ${myEmotion}`)
 }
 addButton.addEventListener("click", addDiary)
-// diaryText.addEventListener("input", disabledAddButton)
 
-// const disabledAddButton = () => {
-//     addButton.disabled = diaryText.value.length == 0
-//     deleteButton.disabled = diaryText.value.length == 0
-// }
+const getSelectedEmotion = () => {
+    emotionContent.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            myEmotion = event.target.textContent
+        }
+    })
+}
+getSelectedEmotion()
+
+function render(diary, emotion) {
+    let resultHTML = " "
+    resultHTML += `${diaryObject},${myEmotion}`
+    document.getElementById("renderArea").innerHTML =resultHTML
+}

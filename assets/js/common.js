241c2932c0469af66fdbcf59f235563b0af73897
김효은 <hyoeun@gmail.com>
2024-03-01 22:48:39 +0900

    header,footer 추가 작업

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,18 +1,4 @@
 
-window.onload = function() {
-    fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    });
-
-    fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;
-    });
-}
-
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var weatherLogo = document.querySelector('.weather_logo');
@@ -21,11 +7,9 @@ window.addEventListener('scroll', function() {
 
 
     if (scrollPosition > 0) {
-        weatherLogo.innerHTML = `<img src="../weatherdiary/assets/image/symbol.svg"/>`;
         header.classList.add('scroll');
         topCategory.classList.add('scroll');
     } else { // 그 외의 경우
-        weatherLogo.innerHTML = `<img src="../weatherdiary/assets/image/logo.svg"/>`;
         header.classList.remove('scroll');
         topCategory.classList.remove('scroll');
     }

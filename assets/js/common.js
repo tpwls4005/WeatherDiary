be3a737dd461ad03b876fbe053aa29ef05208d50
김효은 <hyoeun@gmail.com>
2024-03-01 22:00:10 +0900

    작업 완료

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,18 @@
+
+window.onload = function() {
+    fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+
+    fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+}
+
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var weatherLogo = document.querySelector('.weather_logo');
@@ -15,17 +30,3 @@ window.addEventListener('scroll', function() {
         topCategory.classList.remove('scroll');
     }
 });
-
-window.onload = function() {
-    fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    });
-
-    fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;
-    });
-}

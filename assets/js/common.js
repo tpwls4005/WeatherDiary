ef17524439f27a60c389808997817cabd0d1cab0
김효은 <hyoeun@gmail.com>
2024-03-02 16:37:09 +0900

    헤더, 푸터 작업 내용 반영

--- assets/js/common.js
+++ assets/js/common.js
@@ -14,3 +14,48 @@ window.addEventListener('scroll', function() {
         topCategory.classList.remove('scroll');
     }
 });
+let inputWrap = document.querySelector('.alarm_popup_wrap');
+let dim = document.querySelector('.dim');
+
+const openAlarmBox = () => {
+    if (inputWrap.classList.contains("on")) {
+        inputWrap.classList.remove("on");
+    } else {
+        inputWrap.classList.add("on");
+    }
+};
+const dimClose = () => {
+    inputWrap.classList.remove("on");
+}
+
+// 시간 선택 셀렉트 박스
+const hourSelect = document.getElementById('hourSelect');
+const minuteSelect = document.getElementById('minuteSelect');
+
+// 시간 선택 박스 생성
+for(let i=0; i<24; i++) {
+    let hour = i < 10 ? '0' + i : i;
+    let hourOption = document.createElement('option');
+    hourOption.text = hour;
+    hourOption.value = hour;
+    hourSelect.add(hourOption);
+}
+
+// 분 선택 박스 생성
+for(let j=0; j<60; j+=5) {
+    let minute = j < 10 ? '0' + j : j;
+    let minuteOption = document.createElement('option');
+    minuteOption.text = minute;
+    minuteOption.value = minute;
+    minuteSelect.add(minuteOption);
+}
+
+const confirmAlarmBox = () => {
+    alert('알림이 설정되었어요!! 설정한 시간: ' + hourSelect.value + '시 ' + minuteSelect.value + '분');
+    inputWrap.classList.remove("on");
+}
+const closeAlarmBox = () => {
+    alert('알림 설정을 취소하시겠습니까?');
+    inputWrap.classList.remove("on");
+}
+
--- assets/js/sub.js
+++ assets/js/sub.js
@@ -3,11 +3,35 @@ window.onload = function() {
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
+       
+        // header가 로드된 후에 이벤트 설정
+        var pageTitle = document.querySelector('.page_title');
+        var navItems = document.querySelectorAll('.top_category li');
+
+        navItems.forEach(function(item) {
+            item.classList.remove('on');
+            if (item.textContent.trim() === pageTitle.textContent.trim()) {
+                item.classList.add('on');
+            }
+        });    
     });
 
     fetch('footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
+
+        // header가 로드된 후에 이벤트 설정
+        var pageTitle = document.querySelector('.page_title');
+        var footNavItems = document.querySelectorAll('.foo_list li');
+
+        footNavItems.forEach(function(item) {
+            item.classList.remove('on');
+            if (item.textContent.trim() === pageTitle.textContent.trim()) {
+                item.classList.add('on');
+            }
+        });    
+        
+        
     });
 }
